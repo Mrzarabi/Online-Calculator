b8af49bb7f2b895caa67529314340cfdf284df49
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-10-14 06:29:29 +0330

    fixing the include route pages

--- resources/views/v1/panel/layouts/ticket/ticket.blade.php
+++ resources/views/v1/panel/layouts/ticket/ticket.blade.php
@@ -3,10 +3,10 @@
         
     @if ($starter->closed == false)
         {{-- create ticket --}}
-        @include('V1.panel.layouts.ticket.create-ticket')
+        @include('v1.panel.layouts.ticket.create-ticket')
     @endif
 
     {{-- show tickets --}}
-    @include('V1.panel.layouts.ticket.show-tickets')
+    @include('v1.panel.layouts.ticket.show-tickets')
 
 @endsection

d9918d4712cb0b0fdcdd505044c538bed9ac8595
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-10-14 03:55:53 +0330

    create the function close

--- app/Http/Controllers/V1/Panel/StarterController.php
+++ app/Http/Controllers/V1/Panel/StarterController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\V1\Panel;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\Search\SearchRequest;
 use App\Models\Starter;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StarterController extends Controller
 {
@@ -89,5 +91,46 @@ class StarterController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function close(Starter $starter)
+    {
+        if(auth()->user()->isAbleto('ticket-read')) {
+
+            DB::transaction(function () use($starter) {
+                $starter->update([
+                    'closed' => true
+                ]);
+            });
+            return redirect()->back();
+        } else {
+
+            abort(403, 'Forbidden.');
+        }
+    }
+
+    public function search(SearchRequest $request)
+    {
+        // Search only active users
+        if(auth()->user()->hasRole('100e82ba-e1c0-4153-8633-e1bd228f7399')) {
+            
+            if ($request->has('search')) {
+     
+                $starts = Starter::search($request->get('search'))->with('user')->latest()->paginate(9);
+            } else {
+                
+                $starts = Starter::with('user')->latest()->paginate(9);
+            }
+
+            return view('v1.panel.layouts.start.starts', compact('starts'));
+        } else {
+
+            abort(403, 'Forbidden.');
+        }
     }
 }

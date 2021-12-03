eafcf4d453e31dad93804a86906041e7ee20797c
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-12-03 22:30:55 +0330

    add some new styles and divs

--- resources/views/v1/customer/layouts/order/orders.blade.php
+++ resources/views/v1/customer/layouts/order/orders.blade.php
@@ -7,11 +7,28 @@
 @endphp
 @extends('adminlte::page')
 @section('content')
+
+    
+
     {{-- card view orders --}}
     @include('v1.customer.layouts.order.card-view-orders')
 
-    {{-- table view orders --}}
-    @include('v1.customer.layouts.order.table-view-orders')
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-9  custom-background-card custom-card">
+                {{-- create new order and return to the page index --}}
+                @include('v1.customer.layouts.order.create-order')
+
+                {{-- table view orders --}}
+                @include('v1.customer.layouts.order.table-view-orders')
+            </div>
+            <div class="col-md-3">
+                <div class="d-flex justify-content-end">
+                    <img src="/defaultImages/Right.png" alt="vector" height="780">
+                </div>
+            </div>
+        </div>
+    </div>
     
     <div class="d-flex justify-content-center show-table">
         {!! $orders->render('/vendor.pagination.bootstrap-4') !!}

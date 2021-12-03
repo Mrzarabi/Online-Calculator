56d9c04dcf25b9693a070b9d585c82334193bd19
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-12-03 22:34:43 +0330

    delete extra space

--- resources/views/v1/customer/layouts/ticket/show-tickets.blade.php
+++ resources/views/v1/customer/layouts/ticket/show-tickets.blade.php
@@ -6,5 +6,6 @@
 {{-- table view tickets --}}
 @include('v1.customer.layouts.ticket.show-table-view-tickets')
 
+
 {{-- card view tickets  --}}
 @include('v1.customer.layouts.ticket.show-card-view-tickets')

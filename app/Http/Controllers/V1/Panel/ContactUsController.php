9197e702c6daf04b46641356d7077c7195c37164
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-12-19 18:29:29 +0330

    write contact controller for admin

---
+++ app/Http/Controllers/V1/Panel/ContactUsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\V1\Panel;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\ContactUs;
+
+class ContactUsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if(auth()->user()->isAbleTo('contactUs-read')) {
+
+            $contactUses = ContactUs::latest()->paginate(7);
+            return view('v1.panel.layouts.contactUs.contactUs', compact('contactUses'));
+        } else {
+
+            abort(403, 'Forbidden.');
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ContactUs $contactUs)
+    {
+        if(auth()->user()->isAbleTo('contactUs-delete')) {
+
+            $contactUs->delete();
+            $this->custom_alert('The Message', 'Deleted');
+            return redirect()->back();
+            
+        } else {
+
+            abort(403, 'Forbidden.');
+        }
+    }
+}

bd1930179e1ad360fb699b2a5d3636822520324f
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-12-19 18:36:51 +0330

    create factory starter

---
+++ database/factories/StarterFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Starter;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StarterFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    protected $model = Starter::class;
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->title(),
+            'start_no' => $this->faker->numberBetween(100, 10000),
+            'closed' => $this->faker->boolean(),
+            'answerd' => $this->faker->boolean(),
+        ];
+    }
+}

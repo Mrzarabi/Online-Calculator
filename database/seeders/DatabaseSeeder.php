1499f864dd0fa71a3dd1835c9e1bd8fdd4e64b79
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-12-19 18:37:36 +0330

    write database seeder for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,15 @@
 namespace Database\Seeders;
 
 use App\Models\Article;
+use App\Models\Calculator;
+use App\Models\contactUs;
+use App\Models\Element;
+use App\Models\Feedback;
+use App\Models\Order;
+use App\Models\Starter;
 use App\Models\Ticket;
 use App\Models\User;
+use Database\Factories\OrderFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,13 +23,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
         $this->call(LaratrustSeeder::class);
-        // $users = User::factory()->has(Article::factory()->count(4))->count(10)->create();
+
+        $users =  \App\Models\User::factory(15)->create();
+        $users->each(function($user) {
+            $user->orders()->saveMany(Order::factory()->count(5))->create([
+                'user_id' => $user->id
+            ])->each(function($order) {
+                $order->feedback()->saveMany(Feedback::factory())->create([
+                    'order_id' => $order->id
+                ]);
+            });
+        });
+    
+
+        $calculators = Calculator::factory(20)->create();
+        $calculators->each(function($calculator) {
+            $calculator->factory()->has(Element::factory()->count(23))->create();
+        });
         
-        // $users->each(function($user) {
-        //     $user->factory()->has(Ticket::factory()->count(8))->create();
-        // });
+        $users->each(function($user) {
+            $user->starters()->saveMany(Starter::factory(), 5)->create([
+                'user_id' => $user->id
+            ])->each(function($starter) use($user) {
+                $starter->tickets()->saveMany(Ticket::factory(), 11)->create([
+                    'starter_id' => $starter->id,
+                    'user_id' => $user->id
+                ]);
+            });
+        });
+
+        $contactUses = contactUs::factory(27)->create();
     }
 }

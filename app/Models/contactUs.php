28eef349902bef096fa2f2d7a62b54e1c842e5eb
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-12-19 18:34:55 +0330

    create new model

--- app/Models/contactUs.php
+++ app/Models/contactUs.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class contactUs extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'body',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    // Relations
 }

3dbda6138de2adbba6e492855d4c0e8ddcedfeca
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-12-19 18:36:20 +0330

    create factory calculator

---
+++ database/factories/CalculatorFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Calculator;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CalculatorFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    protected $model = Calculator::class;
+
+    public function definition()
+    {
+        $input = array(
+            'Cach',
+            'Tether', 
+            'Paypal'
+        );
+
+        return [
+            'name' => array_rand($input),
+        ];
+    }
+}

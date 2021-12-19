753ede3e56861bd094588cdfe659623a93ec6fe2
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-12-19 18:35:03 +0330

    create new model

--- app/Models/feedback.php
+++ app/Models/feedback.php
@@ -5,7 +5,48 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class feedback extends Model
+class Feedback extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'order_id',
+
+        'body',
+        'show',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'show' => 'boolean',
+    ];
+
+    // Relations
+
+    /**
+     * Get the order that owns the feedback
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }

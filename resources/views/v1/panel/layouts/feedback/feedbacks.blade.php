e9b0df3175eb9db047242fbc30e86eeb988bfd3e
Mrzarabi <m.hadi.z.1997@gmail.com>
2021-12-20 06:57:29 +0330

    showing all feedbacks for users in views

---
+++ resources/views/v1/panel/layouts/feedback/feedbacks.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+@section('content')
+    <div class="col-md-12">
+        <div class="custom-background-card p-4 h-indexes">
+            <div class="row">
+                
+                {{-- table view --}}
+                @include('v1.panel.layouts.feedback.table-view-feedbacks')
+                
+                <div class="d-flex justify-content-center show-table">
+                    {!! $feedbacks->render('/vendor.pagination.bootstrap-4') !!}
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    {{-- card veiw --}}
+    @include('v1.panel.layouts.feedback.card-view-feedbacks')
+@endsection
+

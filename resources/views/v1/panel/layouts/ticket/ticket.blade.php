11b413f227957c432f5e76dfd947a034a7a7355e
Mrzarabi <m.hadi.z.1997@gmail.com>
2022-01-06 12:50:40 +0330

    showing all tickets when a starter was close

--- resources/views/v1/panel/layouts/ticket/ticket.blade.php
+++ resources/views/v1/panel/layouts/ticket/ticket.blade.php
@@ -4,10 +4,7 @@
         {{-- create ticket --}}
         @include('v1.panel.layouts.ticket.create-ticket')
     @else 
-        <div class="d-flex justify-content-start">
-            <a href=" {{route('starters.index')}} " >
-                <img src="/defaultImages/panel/ticket/back-left.png" alt="back left">
-            </a>
-        </div>
+        {{-- show tickets blade --}}
+        @include('v1.panel.layouts.ticket.show-tickets')
     @endif
 @endsection

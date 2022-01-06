a4abb762d4c277de951e3f36f88c1d0435d7b595
Mrzarabi <m.hadi.z.1997@gmail.com>
2022-01-06 12:46:54 +0330

    delete the extra field

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,6 @@ class UserFactory extends Factory
             'avatar' => $this->faker->imageUrl(300, 300),
             'address' => $this->faker->address,
             'phone' => $this->faker->phoneNumber,
-            'ip' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password

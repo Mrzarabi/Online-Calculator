b7ae1a850574a22df92101583ce243e7866e198c
Mrzarabi <m.hadi.z.1997@gmail.com>
2022-01-22 04:13:17 +0330

    adding some relations on migration validation and delete the extras

--- database/migrations/2021_04_16_113651_create_orders_table.php
+++ database/migrations/2021_04_16_113651_create_orders_table.php
@@ -20,13 +20,19 @@ class CreateOrdersTable extends Migration
                                     ->onDelete('cascade')
                                     ->onUpdate('cascade');
 
+            $table->foreignId('calculator_id')->references('id')->on('calculators')
+                                    ->onDelete('cascade')
+                                    ->onUpdate('cascade');
+
+            $table->foreignId('element_id')->references('id')->on('elements')
+                                    ->onDelete('cascade')
+                                    ->onUpdate('cascade');
+
             
-            $table->string('input_currency_type')->nullable();
             $table->float('input_number')->nullable();
             $table->string('input_currency_unit')->nullable();
 
             
-            $table->string('output_currency_type')->nullable();
             $table->float('output_number')->nullable();
             $table->string('output_currency_unit')->nullable();
 

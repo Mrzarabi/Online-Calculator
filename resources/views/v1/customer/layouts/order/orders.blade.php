c04e0c743279406dbfa9f856a40468b8b36385bf
Mrzarabi <m.hadi.z.1997@gmail.com>
2022-02-07 18:03:08 +0330

    showing better order

--- resources/views/v1/customer/layouts/order/orders.blade.php
+++ resources/views/v1/customer/layouts/order/orders.blade.php
@@ -1,35 +1,24 @@
-@php
-    use App\Models\Clearing;
-    use App\Models\Form;
-    use App\Models\Calculator;
-    use App\Models\Element;
-    use Carbon\Carbon;
-@endphp
 @extends('adminlte::page')
 @section('content')
 
-    
-
-    {{-- card view orders --}}
-    @include('v1.customer.layouts.order.card-view-orders')
-
     <div class="col-md-12">
         <div class="row">
-            <div class="col-md-9 custom-background-card custom-card h-indexes p-4">
-                {{-- create new order and return to the page index --}}
-                @include('v1.customer.layouts.order.create-order')
+            <div class="col-md-10 col-sm-12">
 
                 {{-- table view orders --}}
                 @include('v1.customer.layouts.order.table-view-orders')
 
-                <div class="d-flex justify-content-start show-table">
+                {{-- card view orders --}}
+                @include('v1.customer.layouts.order.card-view-orders')
+
+                <div class="d-flex justify-content-start">
                     {!! $orders->render('/vendor.pagination.bootstrap-4') !!}
                 </div>
+
             </div>
-            <div class="col-md-3">
-                <div class="d-flex justify-content-end">
-                    <img src="/defaultImages/Right.png" alt="vector" height="780">
-                </div>
+
+            <div class="col-md-2 col-sm-12 display-image-none d-flex align-items-end justify-content-center">
+                <img src="/defaultImages/Right.png" alt="vector" height="500" class="size-image-dashboard">
             </div>
         </div>
     </div>

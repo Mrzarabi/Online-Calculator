581ee8d3e30a0961f65d561ff39f011c4cd1c08c
Mrzarabi <m.hadi.z.1997@gmail.com>
2022-02-14 04:45:57 +0330

    Change some styles

--- resources/views/livewire/view/layouts/index.blade.php
+++ resources/views/livewire/view/layouts/index.blade.php
@@ -1,7 +1,7 @@
 <div class="h100">
     <div class="row h100 form-background">
         <div class="col-md-12">
-            <div class="row h100">
+            <div class="row rh100">
                 <div class="col-md-12 pl-0 h30 custom-grid-inventory">
                     @if (isset($inventory->paypal_inventory))
                         <div class="cml">
@@ -34,11 +34,12 @@
                         @csrf
                         <div class="col-md-12">
                             <div class="row">
-                                <div class="col-md-3 col-sm-12">
-                                    <h6 class="mb-0 text-come text-center site-color d-flex align-self-center"> {{isset($max) ? 'MAX: '.$max : ''}} </h6>
+                                <div class="col-lg-3 col-sm-12 d-flex align-self-center justify-content-center mb-5">
+                                    <h6 class="mb-0 text-come text-center site-color mr-3"> {{isset($max) ? 'MAX: '.$max : ''}} </h6>
+                                    <br>
                                     <h6 class="mb-0 d-block text-come text-center site-color"> {{isset($min) ? 'MIN: '.$min : ''}} </h6>
                                 </div>
-                                <div class="col-md-6 col-sm-12 pr-0 pl-0">
+                                <div class="col-lg-6 col-sm-12 pr-0 pl-0">
                                     <div class="row">
                                         <div class="col-md-12 offset-md-1 col-sm-12">
                                             <h6 class="site-color width-margin-label-form">Send</h6>
@@ -46,9 +47,9 @@
                                         <div class="col-md-12">
                                             <div class="d-flex justify-content-center mw100">
                                                 <div class="form-group">
-                                                    <select name="calculator_id" class="form-control form-control-sm input-currency-type" wire:model.lazy="input_currency_type" required>
+                                                    <select name="calculator_id" class="form-control rounded-left form-control-sm input-currency-type" wire:model.lazy="input_currency_type" required>
                                                         {{-- <img src="/defaultImages/bitcoin-gold-1.png" alt="bitcoin-gold" width="30"> --}}
-                                                        <option selected> Select Currency</option>
+                                                        <option selected> Select Money</option>
                                                         @foreach ($inputs as $input)
                                                             <option class="option-styles" value="{{$input->id}}">{{$input->name}}</option>
                                                         @endforeach
@@ -60,12 +61,12 @@
                                                 <div class="form-group">
                                                     <input type="number" step="0.01" class="form-control form-control-sm mb-2 input-currency-number" id="input_number" min="{{ isset($input->min) ? $input->min : '' }}" max="{{ isset($input->max) ? $input->max : '' }}" placeholder="ENTER THE AMOUNT" name="input_number"  wire:model.lazy="input_number" required>
                                                     @if ($errors->has('input_number'))
-                                                        <span class="d-block text-danger">{{ $errors->first('input_number') }}</span>
+                                                        <span class="d-block text-danger custom-font-size">{{ $errors->first('input_number') }}</span>
                                                     @endif
                                                 </div>
                                                 <div class="form-group">
                                                     <select name="input_currency_unit" class="form-control form-control-sm input-currency-unit" wire:model.lazy="input_currency_unit" required>
-                                                        <option selected>Select Money</option>
+                                                        <option selected>Select Currency</option>
                                                         <option value="USD">USD</option>
                                                         <option value="EUR" disabled>EUR</option>
                                                     </select>
@@ -79,7 +80,7 @@
                                             <div class="d-flex justify-content-center">
                                                 <div class="form-group">
                                                     <select name="element_id" class="form-control form-control-sm output-currency-type" wire:model.lazy="output_currency_type" required>
-                                                        <option selected >Select Currency</option>
+                                                        <option selected >Select Money</option>
                                                         @if (isset($outputs))
                                                             @foreach ($outputs as $output)
                                                                 <option value="{{$output->id}}">{{$output->name}}</option>
@@ -100,9 +101,9 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-md-8 offset-md-2 cpr-sm-0" style="padding-left: 0;">
+                                        <div class="col-md-8 offset-md-2 cpr-sm-0 pl-0">
                                             @guest
-                                                <h6 class="site-color mt-4 text-center" style="font-size: .7rem;">If You Are Interesting To Trade Please, Log In/Sing Up At First</h6>
+                                                <h6 class="site-color mt-4 text-center custom-font-size">If You Are Interesting To Trade Please, Log In/Sing Up At First</h6>
                                             @else
                                                 <div class="d-flex justify-content-center mt-4">
                                                     @if($isDisabled)
@@ -115,7 +116,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-md-3 col-sm-12 accent-green p-0 d-flex align-items-center justify-content-sm-center mt-sm">
+                                <div class="col-lg-3 col-sm-12 accent-green p-0 d-flex align-items-center justify-content-sm-center mt-sm">
                                     <div class="row">
                                         <div class="col-md-12">
                                             <h6 class="mb-0 @if ($image) mt--5 @endif d-block text-come text-center site-color" style="margin-top: -3rem;"> {{isset($cost) ? $cost ."%" : ''}} </h6>
